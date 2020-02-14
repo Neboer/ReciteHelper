398295c9f941e472783e1ff9d29ac035ced4ba64
Neboer <rubinposter@gmail.com>
2020-02-14 10:08:53 +0800

    增加了切换时文字的微弱动效。

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
             pron: "?",
             pingjia: true,
             selectQuestionRange: ["pngl", "lpng", "pial", "lpia"],
-            animClass: {word: "stable", pron: "stable"}
+            animClass: {word: "stable", pron: "stable", number: "stable"}
         };
         this.nextQuesAndRender = this.nextQuesAndRender.bind(this);
         this.previousQuesAndRender = this.previousQuesAndRender.bind(this);
@@ -61,52 +61,66 @@ class App extends React.Component {
     // 加入动画效果
     nextQuesAndRender() {
         // 判断哪个元素需要运动
-        let updater = {word: "next", pron: "next"};
+        let updater = {word: "next", pron: "next", number: "num-next"};
         if (this.state.word === "?" || this.state.pron === "?") {
             // 没有显示答案
             let t = this.paperList[this.current_question_index].Type;
-            this.current_question_index = this.current_question_index + 1;
-            let d = this.paperList[this.current_question_index].Type;
+            // 指针改动不会立即影响指示器显示的值，但是指示器的值会在动画开始的时候被直接修改，非常难受。我给出的解决方案就是暂时不动current question index，
+            // 并在渲染当前状态前修改这个值。
+            // this.current_question_index = this.current_question_index + 1;
+            let d = this.paperList[this.current_question_index + 1].Type;
             let his_qm_pos = t === "pngl" || t === "pial"; // pron是?
             let fur_qm_pos = d === "pngl" || d === "pial";
             if (his_qm_pos === fur_qm_pos) {
                 // 两次问号出现的位置相同 均是true，只更新word，均是false，只更新pron
-                updater = {word: his_qm_pos ? "next" : "stable", pron: !his_qm_pos ? "next" : "stable"}
+                updater = {
+                    word: his_qm_pos ? "next" : "stable",
+                    pron: !his_qm_pos ? "next" : "stable",
+                    number: "num-next"
+                }
             }
         } else {
+            // 如果已经显示答案，那就都给我运动！
             this.current_question_index = this.current_question_index + 1;
         }
         this.setState({animClass: updater});
         setTimeout(function () {
+            this.current_question_index = this.current_question_index + 1;
             this.renderCurrentQuestion();
         }.bind(this), 250);
         setTimeout(function () {
-            this.setState({animClass: {word: "stable", pron: "stable"}})
+            this.setState({animClass: {word: "stable", pron: "stable", number: "stable"}})
         }.bind(this), 500)
     }
 
     previousQuesAndRender() {
-        let updater = {word: "prev", pron: "prev"};
+        let updater = {word: "prev", pron: "prev", number: "num-prev"};
         // 判断哪个元素需要运动
         if (this.state.word === "?" || this.state.pron === "?") {
             let t = this.paperList[this.current_question_index].Type;
-            this.current_question_index = this.current_question_index - 1;
-            let d = this.paperList[this.current_question_index].Type;
+            // 指针改动不会立即影响指示器显示的值，但是指示器的值会在动画开始的时候被直接修改，非常难受。我给出的解决方案就是暂时不动current question index，
+            // 并在渲染当前状态前修改这个值。
+            // this.current_question_index = this.current_question_index - 1;
+            let d = this.paperList[this.current_question_index - 1].Type;
             let his_qm_pos = t === "pngl" || t === "pial"; // pron是?
             let fur_qm_pos = d === "pngl" || d === "pial";
             if (his_qm_pos === fur_qm_pos) {
                 // 两次问号出现的位置相同 均是true，只更新word，均是false，只更新pron
-                updater = {word: his_qm_pos ? "prev" : "stable", pron: !his_qm_pos ? "prev" : "stable"}
+                updater = {
+                    word: his_qm_pos ? "prev" : "stable",
+                    pron: !his_qm_pos ? "prev" : "stable",
+                    number: "num-prev"
+                }
             }
-        } else {
-            this.current_question_index = this.current_question_index + 1;
         }
+        // 在此时，指示器的状态已经改变了，所以在指示器上添加的动效才会没有作用。
         this.setState({animClass: updater});
         setTimeout(function () {
+            this.current_question_index = this.current_question_index - 1;
             this.renderCurrentQuestion();
         }.bind(this), 250);
         setTimeout(function () {
-            this.setState({animClass: {word: "stable", pron: "stable"}})
+            this.setState({animClass: {word: "stable", pron: "stable", number: "stable"}})
         }.bind(this), 500)
     }
 
@@ -145,12 +159,12 @@ class App extends React.Component {
                     />
                     <img src={'./reload.svg'} alt={'good'} className={'reload'} onClick={this.renewPage}/>
                 </div>
-
                 <div id={'card'}>
                     <div onClick={this.showAnswer}>
                         <div id={"linter"}>{this.state.pingjia ? "平假名" : "片假名"}</div>
                         <div id={'progress'}>
-                            {this.current_question_index + 1} / {this.paperList.length}
+                            <strong style={{display: "inline-block"}}
+                                  className={this.state.animClass.number}>{this.current_question_index + 1}</strong> / {this.paperList.length}
                         </div>
                         <div id={'pron'} className={this.state.animClass.pron}>
                             {this.state.pron}
